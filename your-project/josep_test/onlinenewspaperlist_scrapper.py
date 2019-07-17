408abf95b22b0ea29ccbf7bc7860791a3eaf0656
Josep <workwithjosep@gmail.com>
2019-07-17 08:27:17 +0200

    Scrapped newspapers with language

--- your-project/josep_test/onlinenewspaperlist_scrapper.py
+++ your-project/josep_test/onlinenewspaperlist_scrapper.py
@@ -1,10 +1,14 @@
 from requests_html import HTMLSession
+from langdetect import detect
 session = HTMLSession()
 import pandas as pd
-
+import requests
+import re
+import time
+import random
 # https://www.onlinenewspaperlist.com/
 import urllib3
-import requests
+
 from bs4 import BeautifulSoup
 with open('/home/josep/websites/Onlinenewsletter/www.onlinenewspaperlist.com/index.html', 'r') as html_page:
     html_page = html_page.read()
@@ -13,6 +17,7 @@ soup = BeautifulSoup(html_page, 'lxml')
 
 data = soup.findAll('div', {'class': 'col_midle col_97'})
 list_countys = []
+list_countys_webs = []
 for div in data:
     links = div.findAll('a')
     for a in links:
@@ -33,13 +38,60 @@ for x in list_countys:
     soup = BeautifulSoup(html_page2, 'html.parser')
 
     data = soup.findAll('div', {'class': 'topsitelink_contain clr-fix'})
-    list_countys_webs = []
+
     for div in data:
         links = div.findAll('a')
         for a in links:
             url = a.get('href')
             if url.startswith('htt') is True:
                 contents = a.text
+                x = x.replace("onlinenewspaperlist.com/",'' )
+                x = x.replace(".html",'' )
                 var = (url,x)
-                print(var)
+                list_countys_webs.append(var)
+                print(x)
+medias_list = list_countys_webs
 print(list_countys_webs)
+medias_list = set(list_countys_webs)
+
+print(medias_list)
+
+df = pd.DataFrame(medias_list)
+
+df.to_csv('josep.csv', index=False)
+josep_news_txt = open("josep_news_txt.txt", "a")     # Adding text at the end of the file
+
+def get_language(website,country):
+
+    print('here')
+    print(website)
+    url = website
+    try:
+        res = requests.get(url, verify=False, timeout=3)
+        print(res)
+        if res.status_code == 200:
+            html_page = res.content
+            soup = BeautifulSoup(html_page, 'html.parser')
+            print(type(soup))
+            text = list(soup)
+            text =str(text)
+            if text == None:
+                return 'NaN'
+            text = re.sub('<\s*?script[\s\S]*?(/script>)\W|<[^>]*>','', text)
+            text.strip('\n').strip('\r')
+            text.rstrip('\n').rstrip('\r ')
+            text.lstrip('\n').lstrip('\r')
+            val_lang = detect(text)
+            josep_news_txt.write(website + "," + country + "," + val_lang + "\n")
+            time.sleep(random.randint(0,8))
+            return val_lang
+        else:
+            time.sleep(random.randint(0,8))
+            return 'NaN'
+    except:
+        time.sleep(random.randint(0,8))
+        return 'NaN'
+
+df['lg'] = df.apply(lambda row: get_language(row[0],row[1]), axis=1)
+print(df.head(50))
+df.to_csv('josep2.csv', index=False)
